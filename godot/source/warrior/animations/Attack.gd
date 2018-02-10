458eceeb536a0b716657090b285723a240b0276c
Dlean Jeans <dleanjeans@gmail.com>
2018-02-10 22:23:41 +0700

    Randomize attack duration

--- godot/source/warrior/animations/Attack.gd
+++ godot/source/warrior/animations/Attack.gd
@@ -1,5 +1,11 @@
 extends "res://source/warrior/animations/AnimationPlayer.gd"
 
+func _ready():
+	_randomize_attack_duration()
+
+func _randomize_attack_duration():
+	playback_speed += rand_range(-0.025, 0.025)
+
 func play_attack():
 	if not is_playing():
 		attack_facing_side()

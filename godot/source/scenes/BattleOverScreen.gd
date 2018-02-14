4093cc818e3044e9ce91c5d8ce0709a6404c0534
Dlean Jeans <dleanjeans@gmail.com>
2018-02-14 15:30:37 +0700

    Add a button and a panel background to BattleOverScreen

--- godot/source/scenes/BattleOverScreen.gd
+++ godot/source/scenes/BattleOverScreen.gd
@@ -7,6 +7,8 @@ func _ready():
 func win():
 	$Label.text = "VICTORY!"
 	
+	yield(get_tree().create_timer(1), "timeout")
+	
 	match Game.level:
 		1: $DamageBonuses/Level1.show()
 		2:
@@ -15,30 +17,36 @@ func win():
 				$DamageBonuses/Level2/C/Warrior.use_parent_material = true
 		3:
 			$Thanks.show()
-			$Buttons/AgainButton.show()
+			$Buttons/TryAgainButton.show()
+			$Buttons/PlayAgainButton.show()
+			$Buttons/TryAgainButton.grab_focus()
 			return
 	
-	yield(get_tree().create_timer(1), "timeout")
 	$Buttons/NextButton.show()
 	$Buttons/NextButton.grab_focus()
-	$Buttons/AgainButton.show()
+	$Buttons/TryAgainButton.show()
 
 func lose():
 	$Label.text = "DEFEAT!"
 	$DamageBonuses.hide()
 	
 	yield(get_tree().create_timer(1), "timeout")
-	$Buttons/AgainButton.show()
-	$Buttons/AgainButton.grab_focus()
+	$Buttons/TryAgainButton.show()
+	$Buttons/TryAgainButton.grab_focus()
 
-func _on_AgainButton_pressed():
+func _on_TryAgainButton_pressed():
+	_back_to_battlefield()
+
+func _on_PlayAgainButton_pressed():
 	if Game.level == 3:
 		Game.level = 1
-	get_tree().change_scene(Scene.BattlefieldPath)
-	get_tree().paused = false
+	_back_to_battlefield()
 
 func _on_NextButton_pressed():
 	Game.level += 1
+	_back_to_battlefield()
+
+func _back_to_battlefield():
 	get_tree().change_scene(Scene.BattlefieldPath)
 	get_tree().paused = false
 

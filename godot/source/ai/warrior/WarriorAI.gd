a6f6a54fad2000fa61b016199fbebbfc12814231
Dlean Jeans <dleanjeans@gmail.com>
2018-02-14 14:56:31 +0700

    Add a individual thinking lock

--- godot/source/ai/warrior/WarriorAI.gd
+++ godot/source/ai/warrior/WarriorAI.gd
@@ -5,8 +5,9 @@ export(NodePath) var character_path #setget set_character_path
 export(int) var thoughts_per_second = 5
 export(float) var far_distance_threshold = 100
 
-onready var _thinking_interval = 1.0 / thoughts_per_second
+var enable_individual_thinking = false setget set_enable_individual_thinking
 var velocity setget , get_velocity
+onready var _thinking_interval = 1.0 / thoughts_per_second
 
 var _enemy
 
@@ -17,10 +18,13 @@ func get_velocity():
 	return get_character().get_velocity()
 
 func _ready():
+	$Move.set_process(false)
+	$Attack.set_process(false)
 	$Locator.connect("enemy_detected_nearby", self, "_set_enemy")
 
 func _set_enemy(enemy):
 	if _enemy != null and not _enemy.is_dead(): return
+	self.enable_individual_thinking = true
 	
 	_enemy = enemy
 	if enemy is Class.WarriorAI:
@@ -33,7 +37,7 @@ func _on_enemy_died():
 	_enemy = null
 
 func _move_towards(direction):
-	if _enemy == null:
+	if not enable_individual_thinking:
 		get_character().move.at(direction)
 
 func _physics_process(delta):
@@ -54,3 +58,8 @@ func get_locator():
 func set_character_path(path):
 	if path != null:
 		$GetterSetter.set_character(get_node(path))
+
+func set_enable_individual_thinking(enabled):
+	enable_individual_thinking = enabled
+	$Move.set_process(enabled)
+	$Attack.set_process(enabled)
